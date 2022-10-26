d7f4a85d6b79e7c5e9d9f32830d44e6b7ede47a0
kpssnik <kpssdeveloper@gmail.com>
2022-10-26 14:56:50 +0300

    Layout and styling finished

--- src/App.js
+++ src/App.js
@@ -7,33 +7,48 @@ function App() {
         <div className="container">
 
             <div className='main'>
-                <h1 className='title'>Todo App</h1>
 
-                <div className="add-block flex-between">
-                    <input className='add-input' type="text" placeholder='Add your new todo'/>
-                    <button className='add-button'>
+                <h1 className='main__title'>Todo App</h1>
+
+                <div className="todo-add-block">
+                    <input className='todo-add-input' type="text" placeholder='Add your new todo'/>
+                    <button className='main-button todo-add-button'>
                         <FontAwesomeIcon icon={faAdd} className='white-icon fa-xl'/>
                     </button>
                 </div>
 
                 <div className="todo-block">
-                    <div className="todo flex-between">
-                        <p className='todo-text'>todo text</p>
-                        <button className='remove-button'>
+
+                    <div className="todo">
+                        <p className='todo__text'>Buy a new gaming laptop</p>
+                        <button className='main-button todo__remove-button'>
                             <FontAwesomeIcon icon={faTrash} className='white-icon fa-xl'/>
                         </button>
                     </div>
-                    <div className="todo flex-between">
-                        <p className='todo-text'>todo text</p>
-                        <button className='remove-button'>
+                    <div className="todo">
+                        <p className='todo__text'>Complete a previous task</p>
+                        <button className='main-button todo__remove-button'>
                             <FontAwesomeIcon icon={faTrash} className='white-icon fa-xl'/>
                         </button>
                     </div>
+                    <div className="todo">
+                        <p className='todo__text'>Create a video for youtube</p>
+                        <button className='main-button todo__remove-button'>
+                            <FontAwesomeIcon icon={faTrash} className='white-icon fa-xl'/>
+                        </button>
+                    </div>
+                    <div className="todo">
+                        <p className='todo__text'>Create a new portfolio site</p>
+                        <button className='main-button todo__remove-button'>
+                            <FontAwesomeIcon icon={faTrash} className='white-icon fa-xl'/>
+                        </button>
+                    </div>
+
                 </div>
 
-                <div className="footer-block flex-between">
-                    <p>You have 4 pending tasks</p>
-                    <button>Clear all</button>
+                <div className="todo-footer-block">
+                    <p className='todo-pending'>You have 4 pending tasks</p>
+                    <button className='main-button todo-clear-button'>Clear all</button>
                 </div>
             </div>
 
