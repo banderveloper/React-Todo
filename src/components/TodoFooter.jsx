7ad24d79df6bf91d69c016de20e908f871739a31
kpssnik <kpssdeveloper@gmail.com>
2022-10-28 16:29:21 +0300

    Removed unnecessary codeline

--- src/components/TodoFooter.jsx
+++ src/components/TodoFooter.jsx
@@ -3,8 +3,6 @@ import TodoClearButton from "./UI/buttons/TodoClearButton";
 
 const TodoFooter = ({pending, clearTodos}) => {
 
-    document.title = 'Todo (' + pending + ')';
-
     return (
         <div className="todo-footer-block">
             <p className='todo-pending'>You have {pending} pending tasks</p>
